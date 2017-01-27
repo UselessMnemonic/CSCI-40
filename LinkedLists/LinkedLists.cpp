b17e4dbd96dfdce1e7b8ae81076e4684aa403e27
UselessMnemonic <chrisjmadrigal@gmail.com>
2017-01-27 11:25:15 -0800

    meh

--- LinkedLists/LinkedLists.cpp
+++ LinkedLists/LinkedLists.cpp
@@ -1,24 +1,6 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
-
-struct node
-{
-	int data;
-	node* next;
-};
+#include "LinkedLists.h"
 
 /*
-PRINTOUT
-
----------
-|Node ##|
-|(Data )|
----------
-|
-V
-*/
-
 void fillNode(node* entry, int times, int data[])
 {
 	node* current;
@@ -82,21 +64,20 @@ void printNode(node* entry)
 	node* current;
 	current = entry;
 
-	for(int x = 1; x <= times; x++)
+	int x = 1;
+
+	while(current != NULL)
 	{
 		cout<<"---------"<< endl;
 		cout<<"|Node " << setw(2) << x << '|' << endl;
 		cout<<'|' << setw(7) << current->data << '|' << endl;
 		cout<<"---------"<< endl;
 		cout << "|\nV" << endl;
-		
-		if(current->next == NULL)
-		{
-			break;
-		}
-
 		current = current->next;
+		x++;
 	}
+
+	cout << "0" << endl;
 }
 
 int main()
@@ -104,8 +85,92 @@ int main()
 	node* first = createList(10);
 	int data[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	fillNode(first, 10, data);
-	printNode(first, 10);
+	printNode(first);
 	deleteList(first);
 	first = NULL;
 	return 0;
+}
+*/
+
+template <typename T> List::List()
+{
+	size = 0;
+	head = NULL;
+}
+
+template <typename T> List::~List()
+{
+	bool success; //needed to pass to remove
+	while(!isEmpty()) //while the list is not empty, remove item at index 1
+		remove(1, &success);
+}
+
+template <typename T> List::List(const List<T> &otherList)
+{
+	size = otherList.size;
+
+	if(otherList.head == NULL)
+		head = NULL;
+
+	else
+	{
+		head = new ListNode;
+		head->item = otherList->item;
+
+		ListNode *currentNode = head;
+
+		for(ListNode *nodeInOtherList = otherList.head->next; nodeInOtherList != NULL; nodeInOtherList = nodeInOtherList->next)
+		{
+			currentNode->next = new ListNode;
+			currentNode->next->item = nodeInOtherList->item;
+			currentNode = currentNode->next;
+		}
+
+		currentNode->next = NULL;
+	}
+}
+
+template <typename T> bool List::isEmpty() const
+{
+	return (size == 0);
+}
+
+template <typename T> int List::getLength() const
+{
+	return size;
+}
+
+template <typename T> void List::insert(int index, T newItem,bool &success)
+{
+	size++;
+	int newLength = size + 1;
+
+	success = (index >= 1 || index <= size);
+
+	if(success)
+	{
+		ListNode *beforeTargetNode = find(size-1); //get last node
+		beforeTargetNode->next = new ListNode; //set the node pointed to by the last node to a new ListNode
+		beforeTargetNode->next->item = newItem; //set the new node's item to newItem
+		beforeTargetNode->next->next = NULL; //set the node pointed to by the new node to NULL
+	}
+	else
+	{
+
+	}
+}
+
+template <typename T> void List::remove(int index, bool &success)
+{
+
+}
+
+template <typename T> void List::retrieve(int index, T &dataItem, bool &success) const
+{
+
+}
+
+template <typename T> ListNode List::T* find(int index) const
+{
+
 }
---
+++ LinkedLists/LinkedLists.h
@@ -0,0 +1,65 @@
+// *********************************************************
+// Header file ListP.h for the ADT list
+// Pointer-based implementation
+// *********************************************************
+template <typename T> class List
+    {
+    public:
+        List();                     // default constructor
+        List(const List & L);    //copy Constructor
+        ~List();                //Destructor
+
+        bool isEmpty() const;
+        // Determines whether a list is empty.
+        // Precondition: None.
+        // Postcondition: Returns true if the list is empty;
+        // otherwise returns false.
+
+        int getLength() const;
+        // Determines the length of a list.
+        // Precondition: None.
+        // Postcondition: Returns the number of items
+        // that are currently in the list.
+
+        void insert(int index, ListItemType newItem,bool & success);
+        // Inserts an item into the list at position index.
+        // Precondition: index indicates the position at which
+        // the item should be inserted in the list.
+        // Postcondition: If insertion is successful, newItem is
+        // at position index in the list, and other items are
+        // renumbered accordingly, and success is true;
+        // otherwise success is false.
+        // Note: Insertion will not be successful if
+        // index < 1 or index > getLength()+1.
+
+        void remove(int index, bool & success);
+        // Deletes an item from the list at a given position.
+        // Precondition: index indicates where the deletion
+        // should occur.
+        // Postcondition: If 1 <= index <= getLength(),
+        // the item at position index in the list is
+        // deleted, other items are renumbered accordingly,
+        // and success is true; otherwise success is false.
+
+        void retrieve(int index, ListItemType & dataItem, bool & success) const;
+        // Retrieves a list item by position.
+        // Precondition: index is the number of the item to
+        // be retrieved.
+        // Postcondition: If 1 <= index <= getLength(),
+        // dataItem is the value of the desired item and
+        // success is true; otherwise success is false.
+
+    private:
+        struct ListNode // a node on the list
+		{
+			T item; // a data item on the list
+			ListNode *next; // pointer to next node
+		}; // end struct
+
+        int size; // number of items in list
+        ListNode *head; // pointer to linked list of items
+
+        ListNode *find(int index) const;
+        // Returns a pointer to the index-th node
+        // in the linked list.
+    };
