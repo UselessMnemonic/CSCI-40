b7f3d36a9f19f31d6773c6f2bf8e81bf714fbbe0
UselessMnemonic <chrisjmadrigal@gmail.com>
2017-02-13 11:44:53 -0800

    started ch5#8

--- Exercises/Ch5.cpp
+++ Exercises/Ch5.cpp
@@ -1,5 +1,41 @@
 /*
 *	Authored by Christopher Madrigal
-*	Chapter 5 - 6th ed - Programming Problem #8
+*	Chapter 5 - 6th ed - Programming Problem #8, Infix Expressions
+*
+*	-Grammar-
+*	<expression> = <term>|<term> + <term>|<term> - <term>
+*	<term> = <factor>|<factor>*<factor>|<factor>/<factor>
+*	<factor> = <letter>|(<expression>)
+*	<letter> = a|b|...|z
 */
+#include <string>
+using namespace std;
 
+string findExpression(string);
+string findTerm(string);
+string findFactor(string);
+
+string findExpression(string exp)
+{
+
+}
+
+string findTerm(string t)
+{
+}
+
+string findFactor(string f)
+{
+	if(isalpha(f.at(0)))
+		return f;
+	else if(f.at(0) == '(')
+	{
+		int indOfClosing = f.find_last_of(')');
+		if(indOfClosing == string::npos)
+			return "";
+		else
+			return findExpression(f.substr(1, indOfClosing-1));
+	}
+	else
+		return "";
+}
