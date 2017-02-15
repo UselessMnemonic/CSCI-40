b07af247c33223f08a7fefbb663ff9739d19edcf
UselessMnemonic <chrisjmadrigal@gmail.com>
2017-02-15 00:52:10 -0800

    no??

--- Exercises/Ch5.cpp
+++ Exercises/Ch5.cpp
@@ -8,34 +8,42 @@
 *	<factor> = <letter>|(<expression>)
 *	<letter> = a|b|...|z
 */
+#include <iostream>
 #include <string>
 using namespace std;
 
-string findExpression(string);
-string findTerm(string);
-string findFactor(string);
+string findExpression(string); //returns an expression string, null if the argument is not an expression
+string findTerm(string); //returns the first occurance of a term in the argument, null if no term can be found
+string findFactor(string); //returns the first occurance of a factor in the argument, null if no factor can be found
+bool isExpression(string); //returns true if the argument is an expression, false otherwise
+
+int main() //entry point for program
+{
+	string exp; //stores user input
+	while (true)
+	{
+		cout << "Enter an expression: "; //prompt for string
+		cin >> exp;
+		cout << isExpression(exp) << endl; //print result of isExpression
+	}
+	return 1;
+}
 
 string findExpression(string exp)
 {
-
 }
 
 string findTerm(string t)
 {
+	
 }
 
 string findFactor(string f)
 {
-	if(isalpha(f.at(0)))
-		return f;
-	else if(f.at(0) == '(')
-	{
-		int indOfClosing = f.find_last_of(')');
-		if(indOfClosing == string::npos)
-			return "";
-		else
-			return findExpression(f.substr(1, indOfClosing-1));
-	}
-	else
-		return "";
+	
+}
+
+bool isExpression(string expr)
+{
+	return !findExpression(expr).empty();
 }
