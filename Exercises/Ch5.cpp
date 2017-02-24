a8f7523f50b60a68404df1199e67877ca06ed939
UselessMnemonic <chrisjmadrigal@gmail.com>
2017-02-23 16:36:36 -0800

    too much overthinking

--- Exercises/Ch5.cpp
+++ Exercises/Ch5.cpp
@@ -26,6 +26,15 @@ string findTerm(string); //returns the first occurance of a term in the argument
 string findFactor(string); //returns the first occurance of a factor in the argument, empty if no factor can be found
 bool isExpression(string); //returns true if the argument is an expression, false otherwise
 
+void debug(string msg)
+{
+	cout << msg << endl;
+}
+void debug(string msg, string val)
+{
+	debug(msg + ": \"" + val + "\"");
+}
+
 int main() //entry point for program
 {
 	string exp; //stores user input
@@ -33,73 +42,162 @@ int main() //entry point for program
 	{
 		cout << "Enter an expression: "; //prompt for string
 		cin >> exp;
-		cout << isExpression(exp) << endl; //print result of isExpression
+		//print result of isExpression
+		if (!isExpression(exp))
+			cout << "in";
+		cout << "valid expression" << endl;
 	}
 	return 1;
 }
 
 string findExpression(string stringContainingExpression)
 {
-	string termString = findTerm(stringContainingExpression); //find the first expression
-	string followingString = termString.substr(termString.length()); //get the string following the first term
-	char operatorCharacter;
+	debug("***FIND EXPRESSION**");
+	if (stringContainingExpression.length() == 2)
+		return "";
+	//holds the possible operator between terms
+	string operatorCharacter;
+
+	//holds the result of findTerm on the argument
+	string termString = findTerm(stringContainingExpression);
+	debug("termString", termString);
+	//substring following the found term in the argument
+	string restOfString = stringContainingExpression.substr(termString.length());
+	debug("restOfString", restOfString);
+	//holds the result of a second term search
 	string nextTerm;
-	string constructedExpression;
+	//holds a reconstructed result
+	string result;
 
-	if(!termString.empty()) //if findTerm was successful
+	//if there is possibly at least one more term to find
+	if (restOfString.length() >= 2)
 	{
-		if(followingString.empty()) //if the following substring is empty, then we are done
-			return termString;
-		else //otherwise
+		//find the operator
+		operatorCharacter = restOfString.substr(0, 1);
+		debug("operatorCharacter", operatorCharacter);
+		//if the operator is either a + or -...
+		if (operatorCharacter.at(0) == '+' || operatorCharacter.at(0) == '-')
 		{
-			operatorCharacter = followingString.at(0); //check the next character after our term
-
-			if(operatorCharacter == '+' || operatorCharacter == '-') //if the character is a + or -
+			//find the next term
+			nextTerm = findFactor(restOfString.substr(1));
+			debug("nextTerm", nextTerm);
+			//if the search fails, the argument contains no valid expression
+			if (nextTerm.empty())
+				return "";
+			else //else, return a reconstructed term,
 			{
-				nextTerm = findTerm(followingString.substr(1)); //check if there is another term
+				result = termString + operatorCharacter + nextTerm;;
+				debug("result", result);
 
-				if(!nextTerm.empty()) //if findTerm was successful
-				{
-					constructedExpression = termString.append(&operatorCharacter) + nextTerm; //construct our expression
-
-					if(constructedExpression.length() == stringContainingExpression.length()) //if our expression matches our grammar, we are done
-						return constructedExpression;
-					else //otherwise, the string is not an expression
-						return "";
-				}
-				else //otherwise, the string is not an expression
+				if (stringContainingExpression.compare(result) == 0)
+					return result;
+				else
 					return "";
 			}
-			else //otherwise, the string is not an expression
-			{
-				return "";
-			}
 		}
+		else //else, the argument contains no valid expression
+			return "";
 	}
-	else //otherwise, string is not expression
-		return "";
+	else
+		return termString;
 }
 
 string findTerm(string stringContainingTerm)
 {
+	debug("***FIND TERM**");
+	//holds the possible operator between factors
+	string operatorCharacter;
+
+	//holds the result of findFactor on the argument
+	string factorString = findFactor(stringContainingTerm);
+	debug("factorString", factorString);
+	//substring following the found factor in the argument
+	string restOfString = stringContainingTerm.substr(factorString.length());
+	debug("restOfString", restOfString);
+	//holds the result of a second factor search
+	string nextFactor;
+
+	//if there is possibly at least one more factor to find
+	if (restOfString.length() >= 2)
+	{
+		//find the operator
+		operatorCharacter = restOfString.substr(0, 1);
+		debug("operatorCharacter", operatorCharacter);
+		//if the operator is either a * or /...
+		if (operatorCharacter.at(0) == '*' || operatorCharacter.at(0) == '/')
+		{
+			//find the next factor
+			nextFactor = findFactor(restOfString.substr(1));
+			debug("nextFactor", nextFactor);
+			//if the search fails, the argument contains no valid term
+			if (nextFactor.empty())
+				return "";
+			else //else, return a reconstructed term
+			{
+				return factorString + operatorCharacter + nextFactor;
+			}
+		}
+	}
+	//else, return the found factor, as it is our term
+	return factorString;
 }
 
 string findFactor(string stringContainingFactor)
 {
-	char beginningCharacter = stringContainingFactor.at(0); //char at beginning of string
-	int indexOfLastPara = stringContainingFactor.find_last_of(')'); //last index of )
+	debug("***FIND FACTOR**");
+	string firstCharacter = stringContainingFactor.substr(0, 1);
+	debug("firstCharacter", firstCharacter);
+	string stringContainingExpression;
+	int indOfClosingPara;
 
-	if(beginningCharacter == '(') //if the first character in stringContainingFactor is ( ...
+	if (firstCharacter.at(0) == '(')
 	{
-		if(indexOfLastPara == string::npos) //if there is no coresponding ), the string is not a factor
-			return "";
+		//get the string following the (
+		stringContainingExpression = stringContainingFactor.substr(1);
+		debug("stringContainingExpression", stringContainingExpression);
+
+		//if we have "<expression>)"
+		if (stringContainingExpression.length() >= 4)
+		{
+			//if there is a closing brace after three characters
+			if (stringContainingExpression.at(3) == ')')
+				indOfClosingPara = 3; //then we found the end of our factor
+			else //otherwise, the embedded expression has a far right closing )
+				indOfClosingPara = stringContainingExpression.find_last_of(')');
+
+			cout << "indOfClosingPara: " << indOfClosingPara << endl;
+
+			//if there is no closing ), the argument is not a valid factor
+			if (indOfClosingPara == string::npos)
+				return "";
+			else
+			{
+				//otherwisem isolate the embedded expression and check for validity
+				stringContainingExpression = stringContainingExpression.substr(0, indOfClosingPara);
+				debug("stringContainingExpression", stringContainingExpression);
+				stringContainingExpression = findExpression(stringContainingExpression);
+				debug("stringContainingExpression", stringContainingExpression);
+				//if the search failed, the argument is not a valid factor
+				if (stringContainingExpression.empty())
+					return "";
+				else
+					return "(" + stringContainingExpression + ")";
+			}
+		}
+		else if (stringContainingExpression.length() == 2 && isalpha(stringContainingExpression.at(0))) //however, if it is the case we have "<letter>)"
+		{
+			//then it's implied the argument is "(<letter>)"
+				return stringContainingFactor;
+		}
 		else
-			return "("+findExpression(stringContainingFactor.substr(1,indexOfLastPara))+")"; //if there is a corresponding ), get the expression between () and return (<expression>);
+			return "";
 	}
-	else if (isalpha(beginningCharacter) && stringContainingFactor.length() == 1) //if the first character in stringContainingFactor is a letter and string is len 1...
-		return stringContainingFactor; //string is already a factor, return it
+	else if (isalpha(firstCharacter.at(0)))
+	{
+		return firstCharacter;
+	}
 	else
-		return ""; //if all else, string is not a factor
+		return "";
 }
 
 bool isExpression(string stringContainingExpression)
